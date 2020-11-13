b97f2d58f1b4dc122bf17f092945878f5070183e
Ashlie Ossege <69567292+AJOssege@users.noreply.github.com>
2020-11-12 23:11:39 -0500

    Update Diabetes Project.R

--- Diabetes Project.R
+++ Diabetes Project.R
@@ -1,4 +1,5 @@
 library(faraway)
+library(psych)
 
 # read in diabetes data
 # do exploratory
@@ -14,38 +15,5 @@ plot(diabetes$location)
 plot(diabetes$gender)
 plot(diabetes$frame)
 
-# means 
-mean(diabetes$chol, na.rm = TRUE)
-mean(diabetes$stab.glu, na.rm = TRUE)
-mean(diabetes$hdl, na.rm = TRUE)
-mean(diabetes$ratio, na.rm = TRUE)
-mean(diabetes$glyhb, na.rm = TRUE)
-mean(diabetes$age, na.rm = TRUE)
-mean(diabetes$height, na.rm = TRUE)
-mean(diabetes$weight, na.rm = TRUE)
-mean(diabetes$bp.1s, na.rm = TRUE)
-mean(diabetes$bp.1d, na.rm = TRUE)
-mean(diabetes$bp.2s, na.rm = TRUE)
-mean(diabetes$bp.2d, na.rm = TRUE)
-mean(diabetes$waist, na.rm = TRUE)
-mean(diabetes$hip, na.rm = TRUE)
-mean(diabetes$time.ppn, na.rm = TRUE)
-
-# std dev
-sd(diabetes$chol, na.rm = TRUE)
-sd(diabetes$stab.glu, na.rm = TRUE)
-sd(diabetes$hdl, na.rm = TRUE)
-sd(diabetes$ratio, na.rm = TRUE)
-sd(diabetes$glyhb, na.rm = TRUE)
-sd(diabetes$age, na.rm = TRUE)
-sd(diabetes$height, na.rm = TRUE)
-sd(diabetes$weight, na.rm = TRUE)
-sd(diabetes$bp.1s, na.rm = TRUE)
-sd(diabetes$bp.1d, na.rm = TRUE)
-sd(diabetes$bp.2s, na.rm = TRUE)
-sd(diabetes$bp.2d, na.rm = TRUE)
-sd(diabetes$waist, na.rm = TRUE)
-sd(diabetes$hip, na.rm = TRUE)
-sd(diabetes$time.ppn, na.rm = TRUE)
-
-
+#descriptive Statistics
+describe(diabetes)
