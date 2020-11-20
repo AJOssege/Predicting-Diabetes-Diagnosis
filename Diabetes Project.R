d35ca2191b96b0aad54a4d13fa10baf6b86d3d28
Ashlie Ossege <69567292+AJOssege@users.noreply.github.com>
2020-11-19 20:09:25 -0500

    Added additional predictor code

--- Diabetes Project.R
+++ Diabetes Project.R
@@ -10,6 +10,9 @@ data(diabetes)
 # view data frame
 View(diabetes)
 
+
+diabetes_data<- diabetes
+
 # plots of categoricals
 plot(diabetes$location, main="Location Samples", ylab="Count", col="orange")
 plot(diabetes$gender, main="Gender", ylab="Count", col="green")
@@ -18,3 +21,10 @@ plot(diabetes$frame,  main= "Body Frame", ylab="Count", col="blue" )
 
 #descriptive Statistics
 describe(diabetes)
+
+#Creating additional predictors
+
+diabetes_data$hasDiabetes<-ifelse(diabetes_data$glyhb>=7.0,1,0)
+diabetes_data$bmi <- 703*diabetes_data$weight/(diabetes_data$height)^2
+diabetes_data$bmiCat <- ifelse(diabetes_data$bmi <18.5, "Underweight",ifelse(diabetes_data$bmi <25, "Normal",ifelse(diabetes_data$bmi <30, "Overweight","Obese")))
+
