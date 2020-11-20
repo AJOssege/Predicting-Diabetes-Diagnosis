cec459e5680af46b07b30db9209c26ddc2872036
Ashlie Ossege <69567292+AJOssege@users.noreply.github.com>
2020-11-19 20:40:42 -0500

    Updating data set name to diabetes_data

--- Diabetes Project.R
+++ Diabetes Project.R
@@ -14,13 +14,13 @@ View(diabetes)
 diabetes_data<- diabetes
 
 # plots of categoricals
-plot(diabetes$location, main="Location Samples", ylab="Count", col="orange")
-plot(diabetes$gender, main="Gender", ylab="Count", col="green")
-plot(diabetes$frame,  main= "Body Frame", ylab="Count", col="blue" )
+plot(diabetes_data$location, main="Location Samples", ylab="Count", col="orange")
+plot(diabetes_data$gender, main="Gender", ylab="Count", col="green")
+plot(diabetes_data$frame,  main= "Body Frame", ylab="Count", col="blue" )
 
 
 #descriptive Statistics
-describe(diabetes)
+describe(diabetes_data)
 
 #Creating additional predictors
 
