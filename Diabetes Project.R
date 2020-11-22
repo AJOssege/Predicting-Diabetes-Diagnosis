81b89792ca2c2c5cecc244137324b7cdbbb30cb7
Ashlie Ossege <69567292+AJOssege@users.noreply.github.com>
2020-11-22 01:20:38 -0500

    Added Missing Value Analysis

--- Diabetes Project.R
+++ Diabetes Project.R
@@ -39,4 +39,22 @@ p +geom_bar()
 #descriptive Statistics
 describe(diabetes_data)
 
+#Missing values analysis
+any(is.na(diabetes_data))
+sum(is.na(diabetes_data))
+sum(complete.cases(diabetes_data))
+na_count <-sapply(diabetes_data, function(y) sum(length(which(is.na(y)))))
+na_count <- data.frame(na_count)
+
+myvars<-c("glyhb","frame")
+newdata <-diabetes_data[myvars]
+
+# Missing values analysis Summary:
+# bp.2s and bp.2d have over half sample as NA. 
+# Could the absence of data for the second systolic and diastolic BP be important?
+# Why do so many not have a second BP?
+# 13 cases do not have 'hasdiabetes' value due to missing glyhb
+# 12 cases are missing "frame". Can this be predicted or filled in with mean? 
+# frame and glyhb do not have NA in same rows
+
 
